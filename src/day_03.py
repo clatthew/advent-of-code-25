d7bff2d9409b11548a6d4eecc5fb1854d932d9cd
clatthew <mattjamesclough@gmail.com>
2026-01-09 02:12:09 +0000

    remove debugging stuff

--- src/day_03.py
+++ src/day_03.py
@@ -4,13 +4,8 @@ def get_joltage_of_bank(bank: str, no_batteries=2):
     positions = [-1]
     for i in range(no_batteries, 0, -1):
         candidates = bank_list[positions[-1] + 1 : (-(i - 1) or None)]
-        print(f"{i=}, {candidates=}, {len(candidates)}")
         digits.append(max(candidates))
-        # if i == no_batteries:
-        #     positions[0] = 0
-        print(f"{digits=}, {positions=}")
         positions.append(bank_list.index(digits[-1], positions[-1] + 1))
-        print(f"{digits=}, {positions=}")
     return int("".join(str(i) for i in digits))
 
 
