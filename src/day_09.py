fcb28f406a87b10ceea22ad15329c8a39bb4b4c4
clatthew <mattjamesclough@gmail.com>
2026-01-19 22:00:34 +0000

    day 9 part 2 attempt

--- src/day_09.py
+++ src/day_09.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def prepare_input(input_list):
     return [[int(i) for i in elt.split(",")] for elt in input_list]
 
@@ -9,8 +12,62 @@ def get_rectangle_area(corner_1, corner_2):
 def find_biggest_rectangle(corners):
     max_area = 0
     for i, corner_1 in enumerate(corners):
-        for j, corner_2 in enumerate(corners[1:], i + 1):
+        for corner_2 in corners[i:]:
             area = get_rectangle_area(corner_1, corner_2)
             if area > max_area:
                 max_area = area
     return max_area
+
+
+def get_furthest_corner(corners):
+    max_corner = [0, 0]
+    for corner in corners[::-1]:
+        max_corner[0] = max(max_corner[0], corner[0])
+        max_corner[1] = max(max_corner[1], corner[1])
+    return max_corner
+
+
+def get_blank_floor_map(max_corner, blank="."):
+    return [[blank for _ in range(max_corner[0] + 1)] for _ in range(max_corner[1] + 1)]
+
+
+def fill_between(floor_map, prev_square, new_square):
+    line_index = new_square[0] == prev_square[0]
+    direction = 1 - 2 * (new_square[line_index] < prev_square[line_index])
+    for i in range(
+        prev_square[line_index] + direction,
+        new_square[line_index],
+        direction,
+    ):
+        if line_index:
+            floor_map[i][new_square[1 - line_index]] = "X"
+        else:
+            floor_map[new_square[1 - line_index]][i] = "X"
+    return floor_map
+
+
+def produce_outline(squares):
+    max_corner = get_furthest_corner(squares)
+    floor_map = get_blank_floor_map(max_corner)
+    prev_square = squares[0]
+    floor_map[prev_square[1]][prev_square[0]] = "#"
+    for square in squares[1:]:
+        floor_map[square[1]][square[0]] = "#"
+        floor_map = fill_between(floor_map, prev_square, square)
+        prev_square = square
+    floor_map = fill_between(floor_map, prev_square, squares[0])
+    return floor_map
+
+
+def fill_shape(outline):
+    types = {".": 0, "#": 1, "X": 1}
+    for j, row in enumerate(outline):
+        inside = False
+        for i, cell in enumerate(row):
+            if inside:
+                if not types[cell]:
+                    inside = False
+
+            else:
+                if types[cell]:
+                    inside = True
--- test/test_day_09.py
+++ test/test_day_09.py
@@ -1,6 +1,17 @@
 from pytest import mark, fixture
 from src.day_01 import get_input
-from src.day_09 import prepare_input, get_rectangle_area, find_biggest_rectangle
+from src.day_09 import (
+    prepare_input,
+    get_rectangle_area,
+    find_biggest_rectangle,
+    get_furthest_corner,
+    get_blank_floor_map,
+    fill_between,
+    produce_outline,
+    fill_shape,
+)
+
+from pprint import pprint
 
 
 @fixture
@@ -59,4 +70,206 @@ class Testfind_biggest_rectangle:
     def test_2(self, day_09_input):
         expected = 4737096935
         result = find_biggest_rectangle(day_09_input)
+        assert result == expected
+
+
+class Testget_furthest_corner:
+    @mark.it("gets furthest corner of test data")
+    def test_1(self, test_input):
+        expected = [11, 7]
+        result = get_furthest_corner(test_input)
+        assert result == expected
+
+    @mark.it("gets furthest corner when biggest x and y are in different corners")
+    def test_2(self):
+        test_data = [[11, 3], [4, 7]]
+        expected = [11, 7]
+        result = get_furthest_corner(test_data)
+        assert result == expected
+
+
+class Testget_blank_floor_map:
+    @mark.it("produces correct blank floor map for test data")
+    def test_1(self, test_input):
+        result = get_blank_floor_map(get_furthest_corner(test_input), "!")
+        assert len(result) == 8
+        assert len(result[0]) == 12
+        assert len(result[-1]) == 12
+        assert result[5][6] == "!"
+
+
+@mark.skip
+class Testfill_between:
+    @mark.it("makes outline between two points, forward, horizontal")
+    def test_1(self):
+        corner_0 = [5, 3]
+        corner_1 = [5, 7]
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "#"],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", "X", "X", "X", "#"],
+        ]
+        floor_map[corner_0[0]][corner_0[1]] = "#"
+        result = fill_between(floor_map, corner_0, corner_1)
+        assert result == expected
+
+    @mark.it("makes outline between two points, backward, horizontal")
+    def test_2(self):
+        corner_0 = [5, 3]
+        corner_1 = [5, 7]
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "#"],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", "X", "X", "X", "#"],
+        ]
+        floor_map[corner_0[0]][corner_0[1]] = "#"
+        result = fill_between(floor_map, corner_1, corner_0)
+        assert result == expected
+
+    @mark.it("makes outline between two points, upward")
+    def test_3(self):
+        corner_0 = [5, 3]
+        corner_1 = [1, 3]
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "."],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "."],
+            [".", ".", ".", "X", ".", ".", ".", "."],
+            [".", ".", ".", "X", ".", ".", ".", "."],
+            [".", ".", ".", "X", ".", ".", ".", "."],
+            [".", ".", ".", "#", ".", ".", ".", "."],
+        ]
+        floor_map[corner_0[0]][corner_0[1]] = "#"
+        result = fill_between(floor_map, corner_0, corner_1)
+        assert result == expected
+
+    @mark.it("makes outline between two points, downward")
+    def test_4(self):
+        corner_0 = [0, 6]
+        corner_1 = [4, 6]
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", "X", "."],
+            [".", ".", ".", ".", ".", ".", "X", "."],
+            [".", ".", ".", ".", ".", ".", "X", "."],
+            [".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+        ]
+        floor_map[corner_0[0]][corner_0[1]] = "#"
+        result = fill_between(floor_map, corner_0, corner_1)
+        assert result == expected
+
+
+class Testproduce_outline:
+    @mark.it("produces correct outline for test data")
+    def test_1(self, test_input):
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "X", "X", "X", "#"],
+            [".", ".", ".", ".", ".", ".", ".", "X", ".", ".", ".", "X"],
+            [".", ".", "#", "X", "X", "X", "X", "#", ".", ".", ".", "X"],
+            [".", ".", "X", ".", ".", ".", ".", ".", ".", ".", ".", "X"],
+            [".", ".", "#", "X", "X", "X", "X", "X", "X", "#", ".", "X"],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "X", ".", "X"],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "#", "X", "#"],
+        ]
+        result = produce_outline(test_input)
+        pprint(result)
+        assert result == expected
+
+
+class Testfill_shape:
+    @mark.it("square")
+    def test_1(self):
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", "X", "X", "#", "."],
+            [".", ".", ".", "X", ".", ".", "X", "."],
+            [".", ".", ".", "X", ".", ".", "X", "."],
+            [".", ".", ".", "X", ".", ".", "X", "."],
+            [".", ".", ".", "#", "X", "X", "#", "."],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "#", "X", "X", "#", "."],
+            [".", ".", ".", "X", "X", "X", "X", "."],
+            [".", ".", ".", "X", "X", "X", "X", "."],
+            [".", ".", ".", "X", "X", "X", "X", "."],
+            [".", ".", ".", "#", "X", "X", "#", "."],
+        ]
+        assert fill_shape(floor_map) == expected
+
+    @mark.it("shape with two fill regions on the same row")
+    def test_2(self):
+        floor_map = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            ["#", "X", "#", ".", "#", "X", "#", "."],
+            ["X", ".", "X", ".", "X", ".", "X", "."],
+            ["X", ".", "#", "X", "#", ".", "X", "."],
+            ["X", ".", ".", ".", ".", ".", "X", "."],
+            ["#", "X", "X", "X", "X", "X", "#", "."],
+        ]
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            ["#", "X", "#", ".", "#", "X", "#", "."],
+            ["X", "X", "X", ".", "X", "X", "X", "."],
+            ["X", "X", "#", "X", "#", "X", "X", "."],
+            ["X", "X", "X", "X", "X", "X", "X", "."],
+            ["#", "X", "X", "X", "X", "X", "#", "."],
+        ]
+        result = fill_shape(floor_map)
+        pprint(result)
+        assert result == expected
+
+    @mark.it("fills the test data")
+    def test_3(self, test_input):
+        expected = [
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "X", "X", "X", "#"],
+            [".", ".", ".", ".", ".", ".", ".", "X", "X", "X", "X", "X"],
+            [".", ".", "#", "X", "X", "X", "X", "#", "X", "X", "X", "X"],
+            [".", ".", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            [".", ".", "#", "X", "X", "X", "X", "X", "X", "#", "X", "X"],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "X", "X", "X"],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "#", "X", "#"],
+        ]
+        result = fill_shape(produce_outline(test_input))
+        pprint(result)
         assert result == expected
